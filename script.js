3bdb07b6c356b8d85210309f5e9f2f3afc32d30c
Lara Yassine <laraiyassine@gmail.com>
2025-01-07 00:41:58 -0500

    added javascript functionality

---
+++ script.js
@@ -0,0 +1,15 @@
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+
+  loop: true,
+
+  // If we need pagination
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+
+});

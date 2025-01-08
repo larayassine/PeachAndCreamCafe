b5c2ce01cca6a39ea5725cb252a36fc751841040
Lara Yassine <laraiyassine@gmail.com>
2025-01-08 00:07:40 -0500

    added careers page

--- script.js
+++ script.js
@@ -1,15 +1,10 @@
 const swiper = new Swiper('.swiper', {
-  // Optional parameters
 
   loop: true,
-
-  // If we need pagination
-
-  // Navigation arrows
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
 
-
+effect: 'fade', 
 });
